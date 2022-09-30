b3f6d2c2d4074f99e9a555a45118aa57f7393d13
quanweiZhou <edensky01@163.com>
2022-10-01 05:55:07 +0800

    format lua file

--- lua/init_nvim_tree.lua
+++ lua/init_nvim_tree.lua
@@ -1,5 +1,4 @@
 -- examples for your init.lua
-
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
@@ -9,19 +8,11 @@ require("nvim-tree").setup()
 
 -- OR setup with some options
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
+    sort_by = "case_sensitive",
+    view = {
+        adaptive_size = true,
+        mappings = {list = {{key = "u", action = "dir_up"}}}
     },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    renderer = {group_empty = true},
+    filters = {dotfiles = true}
 })
